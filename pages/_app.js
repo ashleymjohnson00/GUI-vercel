a85de144904947a47adb1ab0a4026165f4af0e3d
ashleymjohnson00 <ashleymjohnson00@tamu.edu>
2023-03-05 17:38:36 -0600

    Changed meta data over to _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import "./style.css";
-
+import Head from 'next/head';
 import React from "react";
-export default function MyApp({
-  Component: Component,
-  pageProps: pageProps
-}) {
+
+export default function MyApp({ Component: Component,
+  pageProps: pageProps }) {
   React.useEffect(() => import("@lottiefiles/lottie-player"));
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" ></meta>
+        <meta charSet="utf-8"></meta>
+        <meta property="twitter:card" content="summary_large_image"></meta>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
