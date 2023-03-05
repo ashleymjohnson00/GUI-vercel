308366b21cfed15effd00f69e2ba0f8025b0f02e
ashleymjohnson00 <ashleymjohnson00@tamu.edu>
2023-03-05 17:56:29 -0600

    Revert "Changed meta data over to _app"
    This reverts commit a85de144904947a47adb1ab0a4026165f4af0e3d.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -150,7 +150,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b606bb65bab58e83"; }
+/******/ 		__webpack_require__.h = function() { return "7edfd6964c5181a0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,10 @@
 import "./style.css";
-import Head from 'next/head';
-import React from "react";
 
-export default function MyApp({ Component: Component,
-  pageProps: pageProps }) {
+import React from "react";
+export default function MyApp({
+  Component: Component,
+  pageProps: pageProps
+}) {
   React.useEffect(() => import("@lottiefiles/lottie-player"));
-  return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" ></meta>
-        <meta charSet="utf-8"></meta>
-        <meta property="twitter:card" content="summary_large_image"></meta>
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />;
 }
