96063d31d1587cdf08c57c85c10e47cb19d07622
ashleymjohnson00 <ashleymjohnson00@tamu.edu>
2023-03-09 21:47:04 -0600

    Update home.js

--- pages/home.js
+++ pages/home.js
@@ -53,8 +53,9 @@ const Home = (props) => {
         </div>
         <div className="home-container05">
           <iframe
-            src="https://www.youtube.com/embed/lwHnvO5QsDc?autoplay=1&origin=http://baja-security.com"
+            src="https://www.youtube.com/embed/lwHnvO5QsDc?autoplay=1&modestbranding=1&origin=http://baja-security.com"
             allow="autoplay"
+            frameborder="0"
             allowFullScreen
             className="home-iframe"
           ></iframe>
