1788c7c212c436725b116317a3f92c5127196538
ashleymjohnson00 <ashleymjohnson00@tamu.edu>
2023-04-12 13:48:06 -0500

    Update new-user.js

--- pages/new-user.js
+++ pages/new-user.js
@@ -24,10 +24,10 @@ const NewUser = (props) => {
       "Content-Type" : "application/json",
       "api-key": mongokey,
     }
-    ).catch((error) => {console.log("failed"); addToast("Account Creation Failed. Try Again :(", { appearance: "error" , autoDismiss: true, autoDismissTimeout: 5000});
+    ).catch((error) => {addToast("Account Creation Failed. Try Again :(", { appearance: "error" , autoDismiss: true, autoDismissTimeout: 5000})
   })
   let displaysuccess = "Account Created Succesfully"
-  addToast("Account Created Succesfully :D", { appearance: "success" , autoDismiss: true, autoDismissTimeout: 3000})
+  addToast("Account Created Succesfully :D", { appearance: "success" , autoDismiss: true, autoDismissTimeout: 4000})
   document.getElementById("displayresultSuccess").innerHTML = displaysuccess
 
   }
