e292d18f0ac25e6f1c98470aaae2c5cf7f317bcd
ashleymjohnson00 <ashleymjohnson00@tamu.edu>
2023-04-12 14:47:17 -0500

    Update new-user.js

--- pages/new-user.js
+++ pages/new-user.js
@@ -29,8 +29,8 @@ const NewUser = (props) => {
     ).catch((error) => {addToast("Account Creation Failed. Try Again :(", { appearance: "error" , autoDismiss: true, autoDismissTimeout: 5000})
   })
   let displaysuccess = "Account Created Succesfully"
-  addToast("Account Created Succesfully :D", { appearance: "success" , autoDismiss: true, autoDismissTimeout: 3000})
-  await timers.setTimeout(3500)
+  addToast("Account Created Succesfully :D", { appearance: "success" , autoDismiss: true, autoDismissTimeout: 5000})
+  await timers.setTimeout(5500)
   document.location.href = "/"
   document.getElementById("displayresultSuccess").innerHTML = displaysuccess
 
