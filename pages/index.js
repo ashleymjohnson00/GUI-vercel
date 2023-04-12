b77d048ed392b83e6e8ff3748c9d66dfc8aaca03
ashleymjohnson00 <ashleymjohnson00@tamu.edu>
2023-04-12 13:03:04 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,6 @@ const Landing = (props) => {
   }
   
   document.getElementById("displayresult").innerHTML = displaysuccess
-  let resultNotif = document.getElementById("displayresult").innerHTML;
   }
   return (
     <>
@@ -81,7 +80,7 @@ const Landing = (props) => {
           className="landing-textinput1 input"
         />
         <button 
-          onClick= {() =>{verifyLogin(); addToast("Trying to Login... ", { appearance: "info" , autoDismiss: true, autoDismissTimeout: 2000}); resultNotif == "Login Failed" ? addToast("Login Failed. Try Again.", { appearance: "error" , autoDismiss: true, autoDismissTimeout: 3000}) : addToast("Login Successful! :D", { appearance: "success" , autoDismiss: true, autoDismissTimeout: 3000});}}  
+          onClick= {() =>{verifyLogin(); addToast("Trying to Login... ", { appearance: "info" , autoDismiss: true, autoDismissTimeout: 2000}); document.getElementById(displaysuccess).innerText === 'Login Failed' ? addToast("Login Failed. Try Again.", { appearance: "error" , autoDismiss: true, autoDismissTimeout: 3000}) : addToast("Login Successful! :D", { appearance: "success" , autoDismiss: true, autoDismissTimeout: 3000});}}  
           id="submitLogin" 
           name="Home button"
         >
