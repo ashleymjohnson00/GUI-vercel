000d244e5a05a922569c6faa4707ad8ad6fc072a
ashleymjohnson00 <ashleymjohnson00@tamu.edu>
2023-04-19 17:10:39 -0500

    Update home.js

--- pages/home.js
+++ pages/home.js
@@ -132,7 +132,12 @@ const Home = (props) => {
         </div>
         <div className="home-container05">
           <iframe
-            seamless src="https://logrocket.com/"
+            src="https://giphy.com/embed/ipta0C5NK3oX8YFLH5"
+            width="100%" height="50%"
+            frameBorder="0" 
+            class="giphy-embed" 
+            allowFullScreen
+            className="home-iframe"
           ></iframe>
           <div className="home-container06">
             <div className="home-container07">
