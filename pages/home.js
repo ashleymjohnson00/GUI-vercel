7184bc7b0c75a335bcf08dcb1412a73cadd53e77
ashleymjohnson00 <ashleymjohnson00@tamu.edu>
2023-05-01 20:41:31 -0500

    Update home.js

--- pages/home.js
+++ pages/home.js
@@ -172,8 +172,8 @@ const Home = (props) => {
                   className="update-info-textinput input"
                   margin="25px"
                 />
-                <br></br>
                 </div>
+                <br></br>
                 <button name="alert clear button" onClick={() => removeAllToasts()}>
                   <a className="alert-clear-button button heading2">Clear Alerts</a>
                 </button>
@@ -549,19 +549,15 @@ const Home = (props) => {
             background-color: #075874;
           }
           .alert-clear-button {
-            color: var(--dl-color-logocolors-origred);
+            color: var(--dl-color-gray-white);
             width: 150px;
             height: 31px;
-            position: absolute;
-            align-self: center;
             text-align: center;
             padding-top: 3px;
-            border-color: var(--dl-color-logocolors-origred);
-            border-width: 2px;
             padding-left: auto;
             border-radius: var(--dl-radius-radius-radius8);
             padding-bottom: var(--dl-space-space-halfunit);
-            background-color: var(--dl-color-gray-white);
+            background-color: var(--dl-color-logocolors-lightred);
           }
           .home-text13 {
             color: var(--dl-color-gray-white);
