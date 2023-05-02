d65acb7295adc87b6a336ef3a00cfd618670a568
ashleymjohnson00 <ashleymjohnson00@tamu.edu>
2023-05-01 19:18:04 -0500

    Update home.js

--- pages/home.js
+++ pages/home.js
@@ -63,7 +63,7 @@ const Home = (props) => {
       setData(ConfidenceInterval)
       console.log("The threat data:", ConfidenceInterval.slice(1))
       if(ConfidenceInterval != previousConfidence){
-        addToast("Warning: Threat Detected!", { appearance: "error" }, { autoDismiss: "True"}, {autoDismissTimeout: "20000"})
+        addToast("Warning: Threat Detected!", { appearance: "error" }/* , { autoDismiss: "True"}, {autoDismissTimeout: "20000"} */)
         var timeStamp = splitFetch[1] + splitFetch[2]
         addUpdateConfidence(splitFetch[0], timeStamp)
         global.time = splitFetch[1]
