4e2eec3f5f9d30f31cfa4ecfd8e612081a85817f
ashleymjohnson00 <ashleymjohnson00@tamu.edu>
2023-05-03 10:00:01 -0500

    Update home.js

--- pages/home.js
+++ pages/home.js
@@ -597,7 +597,8 @@ const Home = (props) => {
            /*  padding-top: 3px; */
             border-width: 0px;
             border-radius: var(--dl-radius-radius-radius8);
-            padding-bottom: 2px; 
+            padding-bottom: 2px;
+            padding-top: 2px; 
             background-color: var(--dl-color-logocolors-lightred);
             box-shadow: 5px 5px 10px 0px rgba(0, 0, 0, 0.28);
           }
